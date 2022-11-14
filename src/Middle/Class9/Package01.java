39b1a433921159d2c032f45dc69dc64c12ba3cbe
RyansRen <ryansren@bestaxc.com.cn>
2022-11-14 18:04:56 +0800

    背包问题遍历顺序

--- src/Middle/Class9/Package01.java
+++ src/Middle/Class9/Package01.java
@@ -1,5 +1,7 @@
 package Middle.Class9;
 
+import java.util.Arrays;
+
 /**
  * Author：Ryans
  * Date：Created in 2022/10/6 18:38
@@ -11,9 +13,9 @@ public class Package01 {
     public static void main(String[] args) {
         int bag = 10;
         int[] weights = new int[] {2, 2, 6, 5, 4};
-        int[] values = {6, 3, 5, 4, 6};
-        System.out.println(packageMoreValue(bag, weights, values));
-        System.out.println(getMaxValue(bag, weights, values));
+        int[] values = {1, 3, 5, 4, 6};
+//        System.out.println(packageMoreValue(bag, weights, values));
+//        System.out.println(getMaxValue(bag, weights, values));
         System.out.println(getMaxValue2(bag, weights, values));
     }
 
@@ -71,10 +73,21 @@ public class Package01 {
     // 一维数组，滚动数组
     private static int getMaxValue2(int bag, int[] weights, int[] values) {
         int[] dp = new int[bag + 1];
+        int[] dp2 = new int[bag + 1];
         for (int i = 0; i < weights.length; i++) {
             for (int j = bag; j >= weights[i]; j--) {
                 dp[j] = Math.max(dp[j], dp[j - weights[i]] + values[i]);
             }
+            System.out.println("dp: " + Arrays.toString(dp));
+        }
+        System.out.println("_____________________");
+        for (int j = bag; j >= 0; j--) {
+            for (int i = 0; i < weights.length; i++) {
+                if(j >= weights[i]) {
+                    dp2[j] = Math.max(dp2[j], dp2[j - weights[i]] + values[i]);
+                }
+            }
+            System.out.println("dp2: " + Arrays.toString(dp2));
         }
         return dp[bag];
     }
--- src/Middle/Class9/PackageWanquan.java
+++ src/Middle/Class9/PackageWanquan.java
@@ -1,5 +1,7 @@
 package Middle.Class9;
 
+import java.util.Arrays;
+
 /**
  * Author : Ryans
  * Date : 2022/11/14
@@ -21,8 +23,9 @@ public class PackageWanquan {
         int len = weights.length;
         int[][] dp = new int[len][bag + 1];
         // 初始化第一行
-        for (int i = 0; i < len; i++) {
-            for (int j = 1; j <= bag; j++) {
+
+        for (int j = 1; j <= bag; j++) {
+            for (int i = 0; i < len; i++) {
                 // 先把上一行的值copy下来
                 if (i > 0) {
                     dp[i][j] = dp[i-1][j];
@@ -34,24 +37,51 @@ public class PackageWanquan {
 
             }
         }
-        for(int i = 0; i < len; i++) {
-            System.out.print("\n");
-            for (int j = 0; j <= bag; j++) {
-                System.out.print(dp[i][j] + "\t");
-            }
-        }
+//        for (int i = 0; i < len; i++) {
+//            for (int j = 1; j <= bag; j++) {
+//                // 先把上一行的值copy下来
+//                if (i > 0) {
+//                    dp[i][j] = dp[i-1][j];
+//                }
+//                // 处理同行问题。
+//                if (j - weights[i] >= 0) {  // 放得下， 放0次，1次。。。
+//                    dp[i][j] = Math.max(dp[i][j], dp[i][j - weights[i]] + values[i]);
+//                }
+//
+//            }
+//        }
+//        for(int i = 0; i < len; i++) {
+//            System.out.print("\n");
+//            for (int j = 0; j <= bag; j++) {
+//                System.out.print(dp[i][j] + "\t");
+//            }
+//        }
         return dp[len - 1][bag];
     }
 
     // 一维数组，压缩空间.
     private static int getMaxValue2(int[] weights, int[] values, int bag) {
         int[] dp = new int[bag + 1];
+        int[] dp2 = new int[bag + 1];
         for (int i = 0; i < weights.length; i++) {
             // 与0-1背包不同的是，这里是从小到大遍历，从而使得物品被添加多次。如 j==2*weight[i]时，dp[j-weight[i]] 即为value[i]的值。这里就相当于算了两次
             for (int j = weights[i]; j <= bag ; j++) {
                 dp[j] = Math.max(dp[j], dp[j - weights[i]] + values[i]);
             }
+            System.out.println(Arrays.toString(dp));
         }
+
+        System.out.println("_______________________");
+        // 先遍历背包容量，后遍历物品
+        for (int j = 1; j <= bag ; j++) {
+            for (int i = 0; i < weights.length; i++) {
+                if (j >= weights[i]) {
+                    dp2[j] = Math.max(dp2[j], dp2[j - weights[i]] + values[i]);
+                }
+            }
+            System.out.println(Arrays.toString(dp2));
+        }
+
         return dp[bag];
     }
 }
